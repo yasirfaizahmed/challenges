851b9a3cff02e7821c04f0845c3fa687c6e1056c
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2024-02-02 20:02:56 +0530

    two sum

---
+++ two_sum.py
@@ -0,0 +1,23 @@
+# flake8: noqa
+from typing import List
+
+class Solution:
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        n = len(nums)
+
+        # Build the hash table
+        for i in range(n):
+            numMap[nums[i]] = i
+
+        # Find the complement
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap and numMap[complement] != i:
+                return [i, numMap[complement]]
+
+        return []
+
+
+print(Solution().twoSum([3,2,4], 6))
+
